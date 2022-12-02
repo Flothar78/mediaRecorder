546a8819c3f4b57ceaa1916a512b9058eb6f79ed
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2022-12-02 16:14:30 +0100

    Add glowing on buttons record, play, stop de apprenants

--- mediaRecorder.js
+++ mediaRecorder.js
@@ -4,19 +4,25 @@ let play = document.getElementById("play");
 let audioChunks = [];
 
 navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+  record.classList.add("blowing-button");
   const mediaRecorder = new MediaRecorder(stream);
   mediaRecorder.addEventListener("dataavailable", (event) => {
     audioChunks.push(event.data);
   });
   record.addEventListener("click", () => {
+    record.classList.remove("blowing-button");
+    stopRecording.classList.add("blowing-button");
     audioChunks = [];
     mediaRecorder.start();
   });
   stopRecording.addEventListener("click", () => {
+    stopRecording.classList.remove("blowing-button");
     mediaRecorder.stop();
+    play.classList.add("blowing-button");
   });
 });
 play.addEventListener("click", (audio) => {
+  play.classList.remove("blowing-button");
   const audioUrl = URL.createObjectURL(new Blob(audioChunks));
   audio = new Audio(audioUrl);
   audio.play();
