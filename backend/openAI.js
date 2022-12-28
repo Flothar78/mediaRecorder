be24651ded409608167e33d5dc6603c3e403d422
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2022-12-28 22:30:38 +0100

    whisper transcribe OK

--- backend/openAI.js
+++ backend/openAI.js
@@ -1,14 +1,31 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "../.env" });
+//const dotenv = require("dotenv");
+//dotenv.config({ path: "../.env" });
+//
+//const { Configuration, OpenAIApi } = require("openai");
+//
+//const configuration = new Configuration({
+//  apiKey: process.env.OPENAI_API_KEY,
+//});
+//const openai = new OpenAIApi(configuration);
+//
+//const completion =  openai.createCompletion({
+//  model: "text-davinci-002",
+//  prompt: "Hello world",
+//});
+//console.log(completion.data.choices[0].text);
 
-const { Configuration, OpenAIApi } = require("openai");
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+ 
+const { exec } = require("child_process");
+
+exec("/home/flothar78/.local/bin/whisper testSon.wav", (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
 });
-const openai = new OpenAIApi(configuration);
-const completion = openai.createCompletion({
-  model: "text-davinci-002",
-  prompt: "Hello world",
-});
-console.log("d'accord");
